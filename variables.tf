0114d81c531d77368bc65b22765eeae915b280a6
robertsweetman <robertsweetman@om.me>
2024-08-18 17:41:33 +0100

    remove un-needed vars

--- variables.tf
+++ variables.tf
@@ -3,15 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2" # You can change this to your preferred region
 }
-
-# We'll define these variables but not set them here
-# They will be set using GitHub Secrets
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
