7e14567bdb122b32f14ad3f6640f39e04a5b43e7
robertsweetman <robertsweetman@om.me>
2024-08-18 19:34:40 +0100

    use correct bucket name

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   # backend state is held in S3
   backend "s3" {
-    bucket = "aws_s3_bucket.terraform_state.id"
+    bucket = "tfstate-rs-${random_string.bucket_suffix.result}"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
