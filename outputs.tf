179e7c796fd6db83edf376a872cc0f988e1b7782
robertsweetman <robertsweetman@om.me>
2024-08-25 17:57:50 +0100

    outputs not ready yet

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,18 @@ output "terraform_state_bucket_name" {
   value       = data.aws_s3_bucket.terraform_state.id
 }
 
-output "lambda_function_arn" {
-  description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.rust_lambda.arn
-}
-
-output "lambda_function_name" {
-  description = "The name of the Lambda function"
-  value       = aws_lambda_function.rust_lambda.function_name
-}
-
 output "lambda_bucket_name" {
   description = "The name of the Lambda bucket"
   value       = aws_s3_bucket.lambda_bucket.id
 }
+
+# output "lambda_function_arn" {
+#   description = "The ARN of the Lambda function"
+#   value       = aws_lambda_function.rust_lambda.arn
+# }
+
+# output "lambda_function_name" {
+#   description = "The name of the Lambda function"
+#   value       = aws_lambda_function.rust_lambda.function_name
+# }
+
