3ee4ce9b0d417470188243d3765184407f4f8e52
robertsweetman <robertsweetman@om.me>
2024-08-25 17:29:53 +0100

    add permissions

--- iam.tf
+++ iam.tf
@@ -1,33 +1,33 @@
-# don't need a duplicate of this
-# resource "aws_iam_policy" "terraform_policy" {
-#   name        = "TerraformPolicy"
-#   path        = "/"
-#   description = "IAM policy for Terraform operations"
+resource "aws_iam_policy" "terraform_policy" {
+  name        = "TerraformPolicy"
+  path        = "/"
+  description = "IAM policy for Terraform operations"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "s3:*",
-#           "lambda:*",
-#           "iam:*",
-#           "cloudwatch:*",
-#           "dynamodb:*"
-#           // Add other necessary services here
-#         ]
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:*",
+          "lambda:*",
+          "iam:*",
+          "cloudwatch:*",
+          "dynamodb:*",
+          "ses:CreateEmailIdentity"
+          // Add other necessary services here
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_user" "terraform_user" {
-#   name = "terraform-user"
-# }
+resource "aws_iam_user" "terraform_user" {
+  name = "terraform-user"
+}
 
-# resource "aws_iam_user_policy_attachment" "terraform_user_policy" {
-#   user       = aws_iam_user.terraform_user.name
-#   policy_arn = aws_iam_policy.terraform_policy.arn
-# }
+resource "aws_iam_user_policy_attachment" "terraform_user_policy" {
+  user       = aws_iam_user.terraform_user.name
+  policy_arn = aws_iam_policy.terraform_policy.arn
+}
