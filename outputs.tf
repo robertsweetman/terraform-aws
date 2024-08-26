3a18d4c45b8bdc906abe3a2346b2b950ef53db41
robertsweetman <robertsweetman@om.me>
2024-08-26 14:50:59 +0100

    change outputs

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,13 @@ output "lambda_bucket_name" {
   value       = aws_s3_bucket.lambda_bucket.id
 }
 
-# output "lambda_function_arn" {
-#   description = "The ARN of the Lambda function"
-#   value       = aws_lambda_function.rust_lambda.arn
-# }
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.rust_lambda.arn
+}
 
-# output "lambda_function_name" {
-#   description = "The name of the Lambda function"
-#   value       = aws_lambda_function.rust_lambda.function_name
-# }
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = aws_lambda_function.rust_lambda.function_name
+}
 
