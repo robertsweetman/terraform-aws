827a78d9448ab523443e91ac782218521c8be6b2
robertsweetman <robertsweetman@om.me>
2024-08-26 15:05:30 +0100

    update rust code

--- messenger/src/main.rs
+++ messenger/src/main.rs
@@ -1,11 +1,26 @@
 use chrono::prelude::*;
 
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
+use chrono::prelude::*;
+
 fn get_current_time() -> String {
     let now = Utc::now();
     now.format("%Y-%m-%d %H:%M:%S UTC").to_string()
 }
 
-fn main() {
+async fn function_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let time = get_current_time();
-    println!("Current time: {}", time);
+    let message = format!("Current time: {}", time);
+    
+    // Log the message
+    println!("{}", message);
+    
+    // Return a response
+    Ok(json!({ "message": message }))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(function_handler)).await
 }
